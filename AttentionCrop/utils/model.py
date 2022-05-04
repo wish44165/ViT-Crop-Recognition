e3a8e961ce0083ee4ff816ea30bf6f9050bf44a1
KevinChen880723 <B10602120@mail.ntust.edu.tw>
2022-05-04 22:55:20 +0800

    Add function: select the activation function from the YAML file

--- AttentionCrop/utils/model.py
+++ AttentionCrop/utils/model.py
@@ -2,6 +2,16 @@ import torch
 import torch.nn as nn
 import segmentation_models_pytorch as smp
 
+class Activation(nn.Module):
+    def __init__(self, act_func_type):
+        super(Activation, self).__init__()
+        if act_func_type == 'LeackyReLU':
+            self.act_func = nn.LeakyReLU()
+        elif act_func_type == 'Mish':
+            self.act_func = nn.Mish()
+    def forward(self, x):
+        return self.act_func(x)
+
 class CroppingModel(nn.Module):
     def __init__(self, cfg):
         super(CroppingModel, self).__init__()
@@ -12,7 +22,8 @@ class CroppingModel(nn.Module):
             classes=32,                      # model output channels (number of classes in your dataset)
             activation=None
         )
-        self.activate_backbone = nn.LeakyReLU()
+        hidden_activation = cfg['model'].get('hidden_activation', 'LeackyReLU')
+        self.activate_backbone = Activation(hidden_activation)
 
         # Initailize the downsampling model
         list_downsample_rate = cfg['model']['list_downsample_rate']
@@ -20,7 +31,7 @@ class CroppingModel(nn.Module):
         length_list = len(list_downsample_rate)
         for i, down_rate in enumerate(list_downsample_rate):
             is_lastlayer = True if i == length_list-1 else False
-            sequence.append(DownSample(32, down_rate, is_lastlayer))
+            sequence.append(DownSample(32, down_rate, hidden_activation, is_lastlayer))
         self.down_sample_model = nn.Sequential(*sequence)
 
     def forward(self, x):
@@ -30,7 +41,7 @@ class CroppingModel(nn.Module):
         return y
 
 class DownSample(nn.Module):
-    def __init__(self, channel_num, downsample_rate, is_lastlayer):
+    def __init__(self, channel_num, downsample_rate, activation_hidden, is_lastlayer):
         super(DownSample, self).__init__()
         if is_lastlayer:
             self.sequence = nn.Sequential(
@@ -41,7 +52,7 @@ class DownSample(nn.Module):
             self.sequence = nn.Sequential(
                 nn.Conv2d(channel_num, channel_num, downsample_rate, downsample_rate),
                 nn.InstanceNorm2d(channel_num),
-                nn.LeakyReLU()
+                Activation(activation_hidden)
             )
 
     def forward(self, x):
